ff30e2ad5fc13cd8c5b6addd68e27e8f97e00881
Bill Randall <william.randall@cru.org>
2018-10-24 09:23:40 -0400

    Add some javadoc

--- core/src/main/java/org/cru/contentscoring/core/service/SyncScoreService.java
+++ core/src/main/java/org/cru/contentscoring/core/service/SyncScoreService.java
@@ -6,6 +6,13 @@ import org.apache.sling.api.resource.ResourceResolver;
 import javax.jcr.RepositoryException;
 
 public interface SyncScoreService {
+    /**
+     * Saves the given {@param score} to the page that was requested. This will then reverse replicate
+     * the change to the author environment, where we actually care about the value.
+     *
+     * @param resourceResolver the subsystem resource resolver (not the request resource resolver)
+     * @param resource the resource on which to save the score
+     */
     void syncScore(
         ResourceResolver resourceResolver,
         int score,
